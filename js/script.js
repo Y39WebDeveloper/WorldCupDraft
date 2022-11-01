d4bdf7dfbb2d7ec1d31c64a67de11b59eda98326
Y39WebDeveloper <y39freelancer@gmail.com>
2022-11-01 17:55:51 +0100

    v 0.3

--- js/script.js
+++ js/script.js
@@ -1,85 +1,96 @@
 let addBtn = document.getElementById("addBtn");
 let teamName = document.getElementById("team");
-let teamCont = document.getElementById("cont");
+let teamContinent = document.getElementById("continent");
 let teamLevel = document.getElementById("lvl");
 let pots = document.querySelectorAll(".levels .level .teams");
 let teams = [];
+let note = "";
 
 addBtn.onclick = addTeam;
-function addTeam(){
+function addTeam() {
     let newTeam = {
         name: teamName.value,
-        cont: teamCont.value,
+        continent: teamContinent.value,
         lvl: teamLevel.value,
     };
-    
+
     let lvlCount = 0;
 
-    for(i = 0; i < teams.length; i++){
-        if(newTeam.lvl == teams[i].lvl){lvlCount++;}
+    for (i = 0; i < teams.length; i++) {
+        if (newTeam.lvl == teams[i].lvl) { lvlCount++; }
     }
 
+    let teamUefa = checkTeam(checkNumberContinent(teams, "europe", 13), lvlCount);
+    let teamCaf = checkTeam(checkNumberContinent(teams, "africa", 5), lvlCount);
+    let teamConmebol = checkTeam(checkNumberContinent(teams, "south_america", 5), lvlCount);
+    let teamConcacaf = checkTeam(checkNumberContinent(teams, "north_america", 3), lvlCount);
+    let teamAfc = checkTeam(checkNumberContinent(teams, "asia", 4), lvlCount);
+    let teamOfc = checkTeam(checkNumberContinent(teams, "oceania", 2), lvlCount);
 
-    let teamUefa = checkTeam(checkNumberCont(teams, "uefa", 13), lvlCount);
-    let teamCaf = checkTeam(checkNumberCont(teams, "caf", 5), lvlCount);
-    let teamConmebol = checkTeam(checkNumberCont(teams, "conmebol", 5), lvlCount);
-    let teamConcacaf = checkTeam(checkNumberCont(teams, "concacaf", 4), lvlCount);
-    let teamAfc = checkTeam(checkNumberCont(teams, "afc-ofc", 5), lvlCount);
-
-    switch (newTeam.cont) {
-        case "uefa":
-            createTeam(teamUefa, newTeam);
+    switch (newTeam.continent) {
+        case "europe":
+            createTeam(teamUefa[0], newTeam, teamUefa[1]);
             break;
-        case "caf":
-            createTeam(teamCaf, newTeam);
+        case "africa":
+            createTeam(teamCaf[0], newTeam, teamCaf[1]);
             break;
-        case "afc-ofc":
-            createTeam(teamAfc, newTeam);
+        case "asia":
+            createTeam(teamAfc[0], newTeam, teamAfc[1]);
             break;
-        case "conmebol":
-            createTeam(teamConmebol, newTeam);
+        case "south_america":
+            createTeam(teamConmebol[0], newTeam, teamConmebol[1]);
             break;
-        case "concacaf":
-            createTeam(teamConcacaf, newTeam);
+        case "north_america":
+            createTeam(teamConcacaf[0], newTeam, teamConcacaf[1]);
+            break;
+        case "oceania":
+            createTeam(teamOfc[0], newTeam, teamOfc[1]);
             break;
     }
-
-    console.log(teams);
     showteam();
 }
 
-function showteam(){
-    for(let i=0; i<4; i++){
+function showteam() {
+    for (let i = 0; i < 4; i++) {
         pots[i].innerHTML = "";
     }
-    for(let i=0; i < teams.length; i++){
-        pots[teams[i].lvl-1].innerHTML += `<li>${teams[i].name}</li>`;
+    for (let i = 0; i < teams.length; i++) {
+        pots[teams[i].lvl - 1].innerHTML += `<li>${teams[i].name}<span onclick="deletTeam(${i})" id="delete">X</span></li>`;
     }
 }
 
-function checkNumberCont(teams, cont, contCounter){
+function checkNumberContinent(teams, continent, continentCounter) {
     let counter = 0;
-    for(let i=0; i < teams.length; i++){
-        if (teams[i].cont == cont){
+    for (let i = 0; i < teams.length; i++) {
+        if (teams[i].continent == continent) {
             counter++;
         }
     }
-    return [cont, counter, contCounter];
+    return [continent, counter, continentCounter];
 }
 
-function checkTeam(checkNumberCont, lvlCount){
+function checkTeam(checkNumberContinent, lvlCount) {
     let check = true;
-    if (lvlCount >= 8 || checkNumberCont[1] >= checkNumberCont[2]){
+    if (lvlCount >= 8) {
         check = false;
-    } else {check = true}
-    return check;
+        note = "You can't add more teams in this level";
+    } else if (checkNumberContinent[1] >= checkNumberContinent[2]){
+        check = false;
+        note = `You can't add more than ${checkNumberContinent[1]} teams from ${checkNumberContinent[0]}`;
+    }else{check = true}
+    return [check, note];
 }
 
-function createTeam(cheking, team){
+function createTeam(cheking, team, note) {
     if (cheking == true) {
         teams.push(team);
         document.querySelector(".note p").innerHTML = "";
-    }else{
-        document.querySelector(".note p").innerHTML = "You can't add more teams from current country !!!";
+    } else {
+        document.querySelector(".note p").innerHTML = note;
     }
+}
+
+function deletTeam(i) {
+    teams.splice(i, 1);
+    showteam();
 }
